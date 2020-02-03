67b81e73f2bc5a3df6d8487237eb5f3f6edc0876
WelshShandy1 <60450096+WelshShandy1@users.noreply.github.com>
2020-02-03 01:58:36 +0000

    Add files via upload

---
+++ init.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $("button1").click(function(){
+    $("p").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("button2").click(function(){
+    $("p").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("button3").click(function(){
+    $("p").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("button4").click(function(){
+    $("p").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("button5").click(function(){
+    $("p").toggle();
+  });
+});
